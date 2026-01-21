0b9b987030b3c957d5e69cca940bda08b8a98ab7
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2026-01-21 19:48:48 +0100

    Pujant branca i estructura

---
+++ src/main/java/com/ra12/projecte1/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ra12.projecte1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+@RequestMapping("/api")
+// nota per jo del futur perque no tinc suficienta part de la api feta per poder fer tests de veritat
+// UserController esta baix com.ra12.projecte1.controller i no com.ra12.projecte1.controller.controller
+// si et trobes problemes potser culpa d'aixo
+public class UserController {
+    
+}
---
+++ src/main/java/com/ra12/projecte1/model/User.java
@@ -0,0 +1,14 @@
+package com.ra12.projecte1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
